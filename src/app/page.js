0b5f8ef12b20b3e6f79ffed53cfb36f6dd1ead6c
Herbertia <117363174+CaracaraEXE@users.noreply.github.com>
2025-08-07 18:44:01 -0300

    testing

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import MoviesGridContainer from "@/app/components/MoviesGridContainer";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="mb-8">Robert Movies night show</h1>
+      <MoviesGridContainer />
+      <p>TESTEANDOOOOOOOOOOOOOOOOO nyan</p>
+    </main>
+  );
+}
